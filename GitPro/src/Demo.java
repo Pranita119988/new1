178347152dc1cdf3dfb60a173184c5cb839e7b7a
Pranita Shidore <shidore.pranita@principal.com>
2019-08-30 15:40:28 +0530

    method reference has been added in java project

--- GitPro/src/Demo.java
+++ GitPro/src/Demo.java
@@ -1,11 +1,12 @@
-import pack1.MyInterfaceImpl;
+import pack1.MyInterface;
+//import pack1.MyInterfaceImpl;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyInterfaceImpl impl=new MyInterfaceImpl();
-		impl.disp();
+		MyInterface ref=System.out::println;
+		ref.disp("hello there");
 	}
 
 }
